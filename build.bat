9b3e5e54eb042b190f8ec8613e357136c1758aed
martincostello <martin@martincostello.com>
2018-04-09 11:59:16 +0100

    Fix build
    Fix the build for more modern versions of Visual Studio.

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Release "OpenRastaSwagger.sln"
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" /m:8 /p:Configuration=Release "OpenRastaSwagger.sln"
 if %errorlevel% neq 0 exit /b %errorlevel%
